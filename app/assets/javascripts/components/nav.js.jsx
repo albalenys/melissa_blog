df387b1f950ad3277bf50bf474cdd8a49fb1224e
Albalenys <albalenysd@gmail.com>
2016-08-01 22:50:31 -0400

    add basic styling to nav

--- app/assets/javascripts/components/nav.js.jsx
+++ app/assets/javascripts/components/nav.js.jsx
@@ -3,10 +3,13 @@ var Nav = React.createClass({
   render: function() {
     return (
       <nav>
-      	<a href='#'>Home</a>
-      	<a href='#'>Category Example</a>
-      	<a href='#'>Category Example</a>
-      	<a href='#'>Category Example</a>
+        <span id='logo'>Melissa</span>
+        <menu>
+        	<a href='#'>Home</a>
+        	<a href='#'>Category Example</a>
+        	<a href='#'>Category Example</a>
+        	<a href='#'>Category Example</a>
+        </menu>
       </nav>
     );
   }
