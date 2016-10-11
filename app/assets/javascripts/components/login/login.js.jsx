ae18788839209c7b6811d9e6b669f991ec80704b
Albalenys <albalenysd@gmail.com>
2016-10-10 21:43:18 -0400

    update image on login page

--- app/assets/javascripts/components/login/login.js.jsx
+++ app/assets/javascripts/components/login/login.js.jsx
@@ -12,7 +12,9 @@ var Login = React.createClass({
     return (
       <main>
         <div id="error">{this.props.error}</div>
-        <img src="https://45.media.tumblr.com/3459ac10c940d6baf94c80009fa6b725/tumblr_mwfg4qcdgD1s4nrjeo1_500.gif" loop="false"/>
+        <div className="center">
+          <img src="http://i126.photobucket.com/albums/p103/julieanne718/animation-cat-sleeping.gif"/>
+        </div>
         <form action="/login" method="post">
           <input name="authenticity_token" type="hidden" value={this.state.token}/>
           <input type="text" name="username" placeholder="username"/>
